440f920d0eab97973c59408c3cc6c08ae0dd5973
LarsEckart <lars.eckart@hey.com>
2022-03-14 20:27:45 +0200

    new attempt at logging

--- src/main/java/org/leanpoker/player/PlayerController.java
+++ src/main/java/org/leanpoker/player/PlayerController.java
@@ -4,9 +4,9 @@ import static org.slf4j.LoggerFactory.getLogger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.spun.util.JsonUtils;
+import com.spun.util.logger.SimpleLogger;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MediaType;
@@ -15,42 +15,42 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.QueryValue;
-import org.leanpoker.player.model.GameState;
 import org.slf4j.Logger;
 
 @Controller()
 public class PlayerController {
 
-    private static final Logger log = getLogger(PlayerController.class);
+  private static final Logger log = getLogger(PlayerController.class);
 
-    ObjectMapper mapper = new ObjectMapper()
-        .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  ObjectMapper mapper = new ObjectMapper()
+      .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
 
-    @Get(produces = MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.ALL)
-    public String doGet() {
-        return "Java player is running";
-    }
+  @Get(produces = MediaType.TEXT_PLAIN)
+  @Consumes(MediaType.ALL)
+  public String doGet() {
+    return "Java player is running";
+  }
 
-    @Post(produces = MediaType.TEXT_PLAIN)
-    @Consumes(MediaType.ALL)
-    public String doPost(@QueryValue String action, @Nullable @QueryValue String game_state, HttpRequest<?> request)
-        throws JsonProcessingException {
-      log.info("request: {}", request.getParameters().get("game_state"));
-        log.info("action {}", action);
-        if (action.equals("bet_request")) {
-            log.info("action game_state {}", game_state);
-            GameState gameState = mapper.readValue(game_state, GameState.class);
-            return String.valueOf(Player.betRequest(gameState));
-        }
-        if (action.equals("showdown")) {
-            Player.showdown(null);
-        }
-        if (action.equals("version")) {
-            return Player.VERSION;
-        }
-        return "";
-    }
+  @Post(produces = MediaType.TEXT_PLAIN)
+  @Consumes(MediaType.ALL)
+  public String doPost(@QueryValue String action, @Nullable @QueryValue String game_state,
+      HttpRequest<?> request)
+      throws JsonProcessingException {
+    SimpleLogger.variable("request", JsonUtils.asJson(request));
+//        log.info("action {}", action);
+//        if (action.equals("bet_request")) {
+//            log.info("action game_state {}", game_state);
+//            GameState gameState = mapper.readValue(game_state, GameState.class);
+//            return String.valueOf(Player.betRequest(gameState));
+//        }
+//        if (action.equals("showdown")) {
+//            Player.showdown(null);
+//        }
+//        if (action.equals("version")) {
+//            return Player.VERSION;
+//        }
+    return "1";
+  }
 
 }
